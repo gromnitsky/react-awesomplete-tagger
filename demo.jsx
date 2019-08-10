0559f08e71dd6653e6c9083338e0c4c4d750f688
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2019-08-11 02:10:35 +0300

    simplify demo

--- demo.jsx
+++ demo.jsx
@@ -1,18 +1,16 @@
 /* global React, ReactDOM, AwesompleteTagger */
 
 class Demo extends React.Component {
-    constructor(props) { super(props) }
-
     render() {
         let opt = { maxItems: 5 } // pass down options to Awesomplete
         return (
-              <AwesompleteTagger type="search" placeholder="poland, portugal"
-                                 completions={this.completions.bind(this)}
-                                 opt={opt} />
+            <AwesompleteTagger type="search" placeholder="poland, portugal"
+                               completions={this.completions}
+                               opt={opt} />
         )
     }
 
-    async completions(user_input) { // simulate a request to a DB
+    completions(user_input) { // simulate a request to a DB
         return fetch('demo.json').then( r => r.json())
     }
 }
